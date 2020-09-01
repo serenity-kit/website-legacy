1bc3da4db89fe16ce2fe56602967672b62f382ec
Nik Graf <nik@nikgraf.com>
2020-09-01 11:20:57 +0200

    initial cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Notes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Lausbub Notes</h1>
+      </main>
+
+      <footer>Made by Lausbub Team</footer>
+    </div>
+  );
+}
