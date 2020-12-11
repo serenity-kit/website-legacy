8ded425734978abfd7cdf9342fcf11d7c448636b
Nik Graf <nik@nikgraf.com>
2020-12-11 16:22:20 +0100

    add support page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -111,6 +111,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <div className="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden space-y-8 sm:px-6 lg:px-8">
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
             <div className="px-5 py-2">
+              <Link href="/en/notes/support">
+                <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                  Support
+                </a>
+              </Link>
+            </div>
+            <div className="px-5 py-2">
               <Link href="/en/notes/privacy-policy">
                 <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
                   Privacy Policy
