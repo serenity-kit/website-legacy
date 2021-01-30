238a69da11efe1f72a6aea38bac93e1603aa30ef
Nik Graf <nik@nikgraf.com>
2021-01-30 20:26:26 +0100

    fix imprint

--- pages/en/notes/imprint.tsx
+++ pages/en/notes/imprint.tsx
@@ -1,32 +1,34 @@
 import * as React from "react";
 import { NextPage } from "next";
-import TextSection from "../../../components/TextSection";
-import { H1 } from "../../../components/Headline";
 
 const ImprintPage: NextPage = () => {
   return (
-    <TextSection>
-      <H1>Imprint</H1>
-      <p>
-        Note: due to Austrian law, we are required to provide this legal
-        document stating who owns the website.
-      </p>
-      <p>
-        Nikolaus Graf
-        <br />
-        Johann Knoll Gasse 9<br />
-        1210 Wien, Österreich
-      </p>
-      <p>hi@serenity.re</p>
-      <p>UID-Nr: ATU66204868</p>
-      <p>Gewerbeordnung: www.ris.bka.gv.at</p>
-      <p>
-        Verbraucher haben die Möglichkeit, Beschwerden an die
-        OnlineStreitbeilegungsplattform der EU zu richten:
-        http://ec.europa.eu/odr. Sie können allfällige Beschwerde auch an die
-        oben angegebene E-Mail-Adresse richten.
-      </p>
-    </TextSection>
+    <div className="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
+      <div className="prose prose-lg text-gray-500 mx-auto">
+        <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-semibold text-gray-900 sm:text-4xl sm:leading-10">
+          Imprint
+        </h1>
+        <p>
+          Note: due to Austrian law, we are required to provide this legal
+          document stating who owns the website.
+        </p>
+        <p>
+          Nikolaus Graf
+          <br />
+          Johann Knoll Gasse 9<br />
+          1210 Wien, Österreich
+        </p>
+        <p>hi@serenity.re</p>
+        <p>UID-Nr: ATU66204868</p>
+        <p>Gewerbeordnung: www.ris.bka.gv.at</p>
+        <p>
+          Verbraucher haben die Möglichkeit, Beschwerden an die
+          OnlineStreitbeilegungsplattform der EU zu richten:
+          http://ec.europa.eu/odr. Sie können allfällige Beschwerde auch an die
+          oben angegebene E-Mail-Adresse richten.
+        </p>
+      </div>
+    </div>
   );
 };
 
