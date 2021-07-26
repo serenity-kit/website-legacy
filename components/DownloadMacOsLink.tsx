05dfee77f189f5c7041aa79a182bf35c9ee0f647
Nik Graf <nik@nikgraf.com>
2021-07-26 06:22:19 +0200

    update macos download link

--- components/DownloadMacOsLink.tsx
+++ components/DownloadMacOsLink.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const DownloadMacOsLink: React.FC<Props> = (props) => (
   <a
-    href="https://github.com/SerenityNotes/serenity-notes-clients/releases/download/v.0.4.0-beta/Serenity.Notes.0.4.0.dmg"
+    href="https://github.com/SerenityNotes/serenity-notes-clients/releases/download/v.0.4.1-beta/Serenity.Notes.0.4.1.dmg"
     className={`no-underline hover:no-underline hover:text-white px-4 py-2 border border-transparent text-lg leading-6 rounded-md text-white bg-black shadow-sm hover:bg-gray-700 active:bg-gray-900 transition duration-150 ease-in-out inline-flex flex-col ${
       props.className ? props.className : ""
     }`}
@@ -15,7 +15,7 @@ const DownloadMacOsLink: React.FC<Props> = (props) => (
       Download
       <br />
     </span>
-    macOS Beta 0.4.0
+    macOS Beta 0.4.1
   </a>
 );
 
