b830ab24d6776bc33efbd4f66aa35eade5613012
Nik Graf <nik@nikgraf.com>
2023-03-15 19:03:08 +0100

    add netidee funding info

--- pages/en/opaque.tsx
+++ pages/en/opaque.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import OpaqueLogo from "../../components/LogoOpaque";
 
 const OpaquePage: NextPage = () => {
@@ -9,10 +10,23 @@ const OpaquePage: NextPage = () => {
           <OpaqueLogo width={232} height={59} />
         </h1>
         <p className="text-center">
-          We will be starting work on <b>Opaque</b> in <b>March 2023</b>. <br />
+          We will be starting work on <b>Opaque</b> in <b>April 2023</b>. <br />
           More details and information can be found{" "}
           <a href="https://github.com/serenity-kit/opaque">here</a>.
         </p>
+        <p className="text-center text-sm">
+          This project was supported by the{" "}
+          <a href="https://www.netidee.at/">Netidee funding campaign</a>.
+        </p>
+        <a href="https://www.netidee.at/">
+          <Image
+            className="mx-auto"
+            src="/opaque/netidee.jpg"
+            alt="Netidee logo"
+            width={125}
+            height={38}
+          />
+        </a>
       </div>
     </div>
   );
